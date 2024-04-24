527b53606a3a157c81cf7d9a251ef0023b254d5b
JWEi <jwei19943@gmail.com>
2024-04-24 23:05:05 +0800

    feat: add fontFamily, minHeight, colors, maxWidth, maxHeight, and darkMode styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,30 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif']
+      },
+      minHeight: {
+        '30': '360px',
+      },
+      colors: {
+        darkModeElements: "hsl(209, 23%, 22%)",
+        darkModeBackground: "hsl(207, 26%, 17%)",
+        lightModeText: "hsl(200, 15%, 8%)",
+        lightModeInput: "hsl(0, 0%, 52%)",
+        lightModeBackground: "hsl(0, 0%, 98%)",
+
+      },
+      maxWidth: {
+        560: "560px",
+      },
+      maxHeight: {
+        400: "400px",
+      },
+    },
   },
+  darkMode: 'class', // or 'media' or 'class'
   plugins: [],
 }
 
